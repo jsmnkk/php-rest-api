a8b1af2f2079808dc1e6c0eec89020af580e58e8
AkhileshNS <nsakhilesh02@gmail.com>
2020-01-09 12:33:07 +0530

    Updated index.php and README.md

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
   use Psr\Http\Message\ServerRequestInterface as Request;
   use Slim\Factory\AppFactory;
   
-  require('vendor/autoload.php');
+  require __DIR__ . '/../vendor/autoload.php';
   
   $app = AppFactory::create();
   
